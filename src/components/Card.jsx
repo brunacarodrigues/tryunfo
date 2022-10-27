d93c6101e7832b04cdb383485543dbf144b69868
brunacarodrigues <brunacris.rodrigues@gmail.com>
2022-10-27 16:46:25 -0300

    requisito 3

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,19 +3,46 @@ import PropTypes from 'prop-types';
 
 class Card extends Component {
   render() {
-    // const {
-    //   cardID,
-    //   cardName,
-    //   cardDescription,
-    //   cardAttr1,
-    //   cardAttr2,
-    //   cardAttr3,
-    //   cardImage,
-    //   cardRare,
-    //   cardTrunfo,
-    // } = this.props;
+    const {
+      // cardID,
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.props;
     return (
-      <p>Teste</p>
+      <>
+        <p data-testid="name-card">
+          {cardName}
+        </p>
+        <img
+          data-testid="image-card"
+          src={ cardImage }
+          alt={ cardName }
+        />
+        <p data-testid="description-card">
+          { cardDescription }
+        </p>
+        <p data-testid="attr1-card">
+          { cardAttr1 }
+        </p>
+        <p data-testid="attr2-card">
+          { cardAttr2 }
+        </p>
+        <p data-testid="attr3-card">
+          { cardAttr3 }
+        </p>
+        <p data-testid="rare-card">
+          { cardRare }
+        </p>
+        <span>
+          { cardTrunfo && <p data-testid="trunfo-card">Super Trunfo</p> }
+        </span>
+      </>
     );
   }
 }
@@ -23,9 +50,18 @@ class Card extends Component {
 Card.propTypes = {
   cardName: PropTypes.string,
   cardDescription: PropTypes.string,
-  cardAttr1: PropTypes.string,
-  cardAttr2: PropTypes.string,
-  cardAttr3: PropTypes.string,
+  cardAttr1: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string,
+  ]),
+  cardAttr2: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string,
+  ]),
+  cardAttr3: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string,
+  ]),
   cardImage: PropTypes.string,
   cardRare: PropTypes.string,
   cardTrunfo: PropTypes.bool,
